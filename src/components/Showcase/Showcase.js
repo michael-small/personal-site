4914a5a8248d856b917fa21e1fe2b7873349d8d5
michael-small <small203@morris.umn.edu>
2021-02-21 18:52:15 -0600

    Create barebones for Showcase page

---
+++ src/components/Showcase/Showcase.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+export default class Showcase extends Component {
+	render() {
+		return (
+			<span className='App'>
+				<h1>Showcase</h1>
+			</span>
+		);
+	}
+}
