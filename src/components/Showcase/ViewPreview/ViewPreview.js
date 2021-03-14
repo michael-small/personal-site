eb2fe2fa066206cb8e8d0e170869504309ddf11c
michael-small <small203@morris.umn.edu>
2021-03-14 14:47:24 -0500

    Add tooltips and message explaining that dev/fam/friends view WIP

--- src/components/Showcase/ViewPreview/ViewPreview.js
+++ src/components/Showcase/ViewPreview/ViewPreview.js
@@ -10,30 +10,56 @@ import DeveloperModeIcon from '@material-ui/icons/DeveloperMode';
 import WorkOutlineIcon from '@material-ui/icons/WorkOutline';
 import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import Tooltip from '@material-ui/core/Tooltip';
 
 export default function ViewShowcase(props) {
+	const [open, setOpen] = React.useState(false);
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
 	return (
 		<Aux>
 			<div id='body'>
 				<div className='center'>
-					<Button
-						variant='outlined'
-						color='primary'
-						size='medium'
-						href={props.link}
-						disabled={props.role === 'Recruiter' ? false : true}
+					<Tooltip
+						open={open}
+						onClose={handleClose}
+						onOpen={handleOpen}
+						title={
+							props.role === 'Recruiter'
+								? 'Open Showcase'
+								: 'Coming soon'
+						}
 					>
-						View as {props.role}
-						{props.role === 'Recruiter' ? (
-							<WorkOutlineIcon />
-						) : props.role === 'Developer' ? (
-							<DeveloperModeIcon />
-						) : props.role === 'Friends and Family' ? (
-							<PeopleOutlineIcon />
-						) : (
-							<HelpOutlineIcon />
-						)}
-					</Button>
+						<span>
+							<Button
+								variant='outlined'
+								color='primary'
+								size='medium'
+								href={props.link}
+								disabled={
+									props.role === 'Recruiter' ? false : true
+								}
+							>
+								View as {props.role}
+								{props.role === 'Recruiter' ? (
+									<WorkOutlineIcon />
+								) : props.role === 'Developer' ? (
+									<DeveloperModeIcon />
+								) : props.role === 'Friends and Family' ? (
+									<PeopleOutlineIcon />
+								) : (
+									<HelpOutlineIcon />
+								)}
+							</Button>
+						</span>
+					</Tooltip>
 				</div>
 				<p>
 					<b>Audience: </b>
