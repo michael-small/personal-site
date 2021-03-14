cbf4aec0d16f7d6609454bea1aee9c922d64f750
michael-small <small203@morris.umn.edu>
2021-03-14 11:58:46 -0500

    Add icons for each view preview role

--- src/components/Showcase/ViewPreview/ViewPreview.js
+++ src/components/Showcase/ViewPreview/ViewPreview.js
@@ -6,22 +6,37 @@ import './ViewShowcase.css';
 // <----- MUI ----->
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
+import DeveloperModeIcon from '@material-ui/icons/DeveloperMode';
+import WorkOutlineIcon from '@material-ui/icons/WorkOutline';
+import PeopleOutlineIcon from '@material-ui/icons/PeopleOutline';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 export default function ViewShowcase(props) {
 	return (
 		<Aux>
 			<div id='body'>
-				<h1 className='view-title'>{props.role} View</h1>
-
-				<p>{props.description}</p>
-				<p>{props.codeBackgroundReq}</p>
 				<div className='center'>
-					<Link to={props.link}>
-						<Button variant='outlined' color='primary'>
-							View as {props.role}
-						</Button>
-					</Link>
+					<Button
+						variant='outlined'
+						color='primary'
+						size='large'
+						href={props.link}
+					>
+						View as {props.role}
+						{props.role === 'Recruiter' ? (
+							<WorkOutlineIcon />
+						) : props.role === 'Developer' ? (
+							<DeveloperModeIcon />
+						) : props.role === 'Friends and Family' ? (
+							<PeopleOutlineIcon />
+						) : (
+							<HelpOutlineIcon />
+						)}
+					</Button>
 				</div>
+				<p>
+					{props.description} {props.codeBackgroundReq}
+				</p>
 				<hr />
 			</div>
 		</Aux>
