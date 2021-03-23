e319351467940acf86d66f409422cf7d5d1b4da4
michael-small <small203@morris.umn.edu>
2021-03-23 01:07:45 -0500

    Create stock contact page with route

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Contact(props) {
+	return (
+		<div>
+			<h1>Contact Information</h1>
+			<h3>LinkedIn</h3>
+			<h3>GitHub</h3>
+			<h3>Email</h3>
+		</div>
+	);
+}
