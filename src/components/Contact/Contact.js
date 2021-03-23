7d82387e1bc97ba3416ebd03d85c8f338c7bcb7f
michael-small <small203@morris.umn.edu>
2021-03-23 01:23:27 -0500

    Center contact subsections in indiv sections with respective MUI logo

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import EmailIcon from '@material-ui/icons/Email';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+import '../UI/_theme.scss';
 
 export default function Contact(props) {
 	return (
 		<div>
 			<h1>Contact Information</h1>
-			<h3>LinkedIn</h3>
-			<h3>GitHub</h3>
-			<h3>Email</h3>
+			<section>
+				<h3 className='centered-text'>
+					LinkedIn <LinkedInIcon />
+				</h3>
+			</section>
+			<section>
+				<h3 className='centered-text'>
+					GitHub <GitHubIcon />
+				</h3>
+			</section>
+			<section>
+				<h3 className='centered-text'>
+					Email <EmailIcon />
+				</h3>
+			</section>
 		</div>
 	);
 }
