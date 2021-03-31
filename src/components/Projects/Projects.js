eb4f6b742163ce4b9778aa49a36c8d351addf90a
michael-small <small203@morris.umn.edu>
2021-03-31 12:37:52 -0500

    Add project descriptions and links to Projects page
    Need to just export the projects basic info from home's projects
    
    Project: 'Create Projects page'

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,22 @@ import React from 'react';
 import theme from '../UI/_theme.scss';
 import Aux from '../hocs/Aux';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import WebIcon from '@material-ui/icons/Web';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		'& > *': {
+			margin: theme.spacing(1),
+		},
+	},
+}));
 
 export default function Projects() {
+	const classes = useStyles();
 	return (
 		<Aux>
 			<Typography variant='h1' className={theme.center}>
@@ -11,14 +25,27 @@ export default function Projects() {
 			</Typography>
 			{/* TODO: Import from home/projects */}
 			<div>
-				<Typography variant='h3' gutterBottom>
-					🧺 Garlicky Bohemian Farm 🚜
-				</Typography>
-				<Typography variant='body1'>
-					A blog and produce showcase for the Garlicky Bohemian Farm,
-					a hobby farm that produces thousands of garlic bulbs per
-					season.
-				</Typography>
+				<div id='witty'>
+					<Typography variant='h3' gutterBottom>
+						🧺 Garlicky Bohemian Farm 🚜
+					</Typography>
+					<Typography variant='body1'>
+						A blog and produce showcase for the Garlicky Bohemian
+						Farm, a hobby farm that produces thousands of garlic
+						bulbs per season.
+					</Typography>
+					<div>
+						<Button
+							variant='contained'
+							color='primary'
+							size='small'
+							href='https://garlickybohemianfarm.com/en'
+							target='_blank'
+						>
+							<pre>Live Demo </pre> <WebIcon />
+						</Button>
+					</div>
+				</div>
 			</div>
 			<div>
 				<Typography variant='h3' gutterBottom>
@@ -29,6 +56,26 @@ export default function Projects() {
 					antiques. No-code interface for editors to add and update
 					antiques. Very much a work in progress.{' '}
 				</Typography>
+				<div className={classes.root}>
+					<Button
+						variant='contained'
+						color='primary'
+						size='small'
+						href='https://eds-garage-sale.herokuapp.com/'
+						target='_blank'
+					>
+						<pre>Live Demo </pre> <WebIcon />
+					</Button>
+					<Button
+						variant='contained'
+						color='primary'
+						size='small'
+						href='https://github.com/michael-small/Eds-Garage-Sale'
+						target='_blank'
+					>
+						<pre>Code </pre> <GitHubIcon />
+					</Button>
+				</div>
 			</div>
 		</Aux>
 	);
