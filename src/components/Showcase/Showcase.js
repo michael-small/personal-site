db1818ab238aa4785c20c76f84c7374b0fffb7d1
michael-small <small203@morris.umn.edu>
2021-05-20 13:05:12 -0500

    Convert `Showcase CC` --> `Showcase FC`
    Addresses #52 (Restructure `/showcase` component hierarchy)
    Projects: 'GraphQL Showcase' and `Showcase View Selection`

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -1,45 +1,33 @@
-import React, { Component } from 'react';
-
-// <----- Components ----->
-import Aux from '../hocs/Aux';
+import React from 'react';
 
 // <----- Styling ----->
 import './Showcase.scss';
 import '../UI/_theme.scss';
 import ViewPreview from './ViewPreview/ViewPreview';
 
-export default class Showcase extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			showCode: false,
-		};
-	}
-
-	render() {
-		return (
-			<div id='showcase' className='center-block'>
-				<h1>Showcase</h1>
-				<h3>Developer and Friends/Family Section coming soon</h3>
-				<ViewPreview
-					role='Recruiter'
-					description='A streamlined overview of some features I want to showcase. If you have hands-on coding experience, consider checking out the developer view.'
-					codeBackgroundReq='No hands-on programming experience required.'
-					link='/showcase/recruiter'
-				/>
-				<ViewPreview
-					role='Developer'
-					description='An in depth walkthrough of features and accompanying explanation of code. Recruiters with hands-on coding experience may want to check this out.'
-					codeBackgroundReq='Web development. Knowledge of React is preferable.'
-					link='/Showcase'
-				/>
-				<ViewPreview
-					role='Friends and Family'
-					description='The simplest overview of the demos. My grandma should be able to understand them <3'
-					codeBackgroundReq='No coding background required.'
-					link='/Showcase'
-				/>
-			</div>
-		);
-	}
+export default function Showcase() {
+	return (
+		<div id='showcase' className='center-block'>
+			<h1>Showcase</h1>
+			<h3>Developer and Friends/Family Section coming soon</h3>
+			<ViewPreview
+				role='Recruiter'
+				description='A streamlined overview of some features I want to showcase. If you have hands-on coding experience, consider checking out the developer view.'
+				codeBackgroundReq='No hands-on programming experience required.'
+				link='/showcase/recruiter'
+			/>
+			<ViewPreview
+				role='Developer'
+				description='An in depth walkthrough of features and accompanying explanation of code. Recruiters with hands-on coding experience may want to check this out.'
+				codeBackgroundReq='Web development. Knowledge of React is preferable.'
+				link='/Showcase'
+			/>
+			<ViewPreview
+				role='Friends and Family'
+				description='The simplest overview of the demos. My grandma should be able to understand them <3'
+				codeBackgroundReq='No coding background required.'
+				link='/Showcase'
+			/>
+		</div>
+	);
 }
